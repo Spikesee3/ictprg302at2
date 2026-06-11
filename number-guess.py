d9b23f0f80853cff927f267cd62e7843baa8bba3
Spikesee3 <81059802+Spiksee3@users.noreply.github.com>
2026-06-11 15:23:06 +1000

    Functions
    added everything into functions and NOW it is done (i think)

--- number-guess.py
+++ number-guess.py
@@ -1,52 +1,64 @@
 import os
 import random
-while True:
+repeatplay = 1
+
+while repeatplay != 0:
     print("--------------------------------------\n| __      __   _                  _  |\n| \\ \\    / /__| |__ ___ _ __  ___| | |\n|  \\ \\/\\/ / -_) / _/ _ \\ '  \\/ -_)_| |\n|   \\_/\\_/\\___|_\\__\\___/_|_|_\\___(_) |\n--------------------------------------\n")
-    name = input("What's your name?: ")
-    number = random.randint(1, 100)
-    attempts = 0
     print(f"The current highscore is: {str(open('highscore.txt').read())} made by {str(open('highscorename.txt').read())}")
-    guess = int(input("Guess a number from 1-100: "))
-    if guess == 9999: # If you enter 9999 it changes the highscore to 99 good for saving time
-        with open('highscore.txt', 'a') as f:
-                f.truncate(0)
-                f.write("99")
-                f.close()
-        print("Changed the high score to 99")
-        continue
-    elif guess > "100":
-        print(f"{guess} is not a valid input, please choose a number from 1-100")
-    elif guess < "1":
-        print(f"{guess} is not a valid input, please choose a number from 1-100")
-    elif guess > number:
-        guess = int(input("Lower "))
-        attempts = attempts + 1
-    elif guess < number:
-        guess = int(input("Higher "))
-        attempts = attempts + 1
-    elif guess == number:
-        print("Congratulations you guessed the number in " + str(attempts) + " tries!")
-        with open("highscore.txt") as g:
-            highscore = float(g.read())
-        if highscore > attempts:
-            if input("Would you like to save your highscore? (y/n): \n") == "y":
-                with open('highscorename.txt', 'a') as f:
-                        f.truncate(0)
-                        f.write(name)
-                        f.close() 
+    name = input("What's your name?: ")
+    repeatplay = 1
+    def numberguess():
+        attempts = 0
+        #number = random.randint(1, 100)
+        number = 3 # remove this before submission and uncomment the bit above
+        guess = int(input("Guess a number from 1-100: "))
+        while True:
+            if guess == 9999: # If you enter 9999 it changes the highscore to 99 good for saving time but remove before submission
                 with open('highscore.txt', 'a') as f:
                         f.truncate(0)
-                        f.write(str(attempts))
-                        f.close()     
-        elif highscore < attempts:
-            print(f"You did not get a highscore, the current highscore is {highscore}") # i forgot what the syntax was for smaller or equal to so i did it twice i know it's ass
-        elif highscore == attempts:
-            print(f"You did not get a highscore, the current highscore is {highscore}")
-        playagain = input("Play again? (y/n): ")
-        if playagain == "y":
-            continue
-        elif playagain == "n":
-            print("Thanks for playing!")
-            exit()
-    else:
-        print(f"{guess} is not a valid number, please choose a number from 1-100")
+                        f.write("99")
+                        f.close()
+                print("Changed the high score to 99")
+                exit()
+            elif guess > number:
+                guess = int(input("Lower "))
+                attempts = attempts + 1
+            elif guess < number:
+                guess = int(input("Higher "))
+                attempts = attempts + 1
+            elif guess == number:
+                print("Congratulations you guessed the number in " + str(attempts) + " tries!")
+                with open("highscore.txt") as g:
+                    highscore = float(g.read())
+                if highscore > attempts:
+                    if input("Would you like to save your highscore? (y/n): \n") == "y":
+                        with open('highscorename.txt', 'a') as f:
+                                f.truncate(0)
+                                f.write(name)
+                                f.close() 
+                        with open('highscore.txt', 'a') as f:
+                                f.truncate(0)
+                                f.write(str(attempts))
+                                f.close()     
+                        break
+                    else:
+                        print("You chose not to save your highscore")
+                        break
+                elif highscore < attempts:
+                    print(f"You did not get a highscore, the current highscore is {highscore}") # i forgot what the syntax was for smaller or equal to so i did it twice i know it's ass
+                    break
+                elif highscore == attempts:
+                    print(f"You did not get a highscore, the current highscore is {highscore}")
+                    break
+            else:
+                print(f"{guess} is not a valid number, please choose a number from 1-100")
+    numberguess()             
+                    
+    def repeatplay():
+            playagain = input("Play again? (y/n): ")
+            if playagain == "y":
+                repeatplay = 0
+            elif playagain == "n":
+                print("Thanks for playing!")
+                exit()
+    repeatplay()
